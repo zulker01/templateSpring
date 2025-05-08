ae09f7e442b43017fc85f5264f8fd8cc077deef3
zulker01 <zulker02@gmail.com>
2025-05-08 17:25:06 +0600

    init

---
+++ src/test/java/com/example/template_spring/TemplateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.template_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
