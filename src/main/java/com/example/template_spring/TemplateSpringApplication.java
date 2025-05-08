59bf8f3669ea29932a60037fecc26dd2388ab96f
zulker01 <zulker02@gmail.com>
2025-05-08 17:26:43 +0600

    init3

--- src/main/java/com/example/template_spring/TemplateSpringApplication.java
+++ src/main/java/com/example/template_spring/TemplateSpringApplication.java
@@ -8,6 +8,7 @@ public class TemplateSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateSpringApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
