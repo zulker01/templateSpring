6d64bf49330bdf193a6dbe1727cd71d0451a7737
zulker01 <zulker02@gmail.com>
2025-07-27 17:48:23 +0600

    deployment done

--- src/main/java/com/example/template_spring/Config/WebConfig.java
+++ src/main/java/com/example/template_spring/Config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.template_spring.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,12 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+  @Value("${frontend.url}")
+  private String frontendUrl;
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     // Allow all origins (you can restrict it to specific origins)
     registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")  // Add your frontend URL here
+            .allowedOrigins(frontendUrl)  // Add your frontend URL here
             .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allow the desired HTTP methods
             .allowedHeaders("*")  // Allow all headers
             .allowCredentials(true)  // Allow credentials (cookies, sessions, etc.)
