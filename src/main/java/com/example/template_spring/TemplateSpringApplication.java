c414c0c8ada3f699c30e9ee503bcc2d519fa6a0d
zulker01 <zulker02@gmail.com>
2025-07-30 18:00:52 +0600

    first github actions commit

--- src/main/java/com/example/template_spring/TemplateSpringApplication.java
+++ src/main/java/com/example/template_spring/TemplateSpringApplication.java
@@ -8,7 +8,7 @@ public class TemplateSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TemplateSpringApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World test github actions v1");
 	}
 
 }
