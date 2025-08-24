0cbbb5019ba1d53a467d69292e2cd8cc67c40e0b
zulker01 <zulker02@gmail.com>
2025-08-24 14:12:01 +0600

    allow options header for preflight requests for REACT js

--- src/main/java/com/example/template_spring/Config/SecurityConfig.java
+++ src/main/java/com/example/template_spring/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.template_spring.Config;
 import com.example.template_spring.Service.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -33,6 +34,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()   // Allow only /api/authy/** without authentication
                         .anyRequest().authenticated()
                 )
